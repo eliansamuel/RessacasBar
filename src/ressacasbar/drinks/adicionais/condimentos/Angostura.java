f7d772c4199db30cd3be482236bbe84139b0ec0a
eliansamuel <eliansamuelsantos@gmail.com>
2021-11-29 05:21:30 -0300

    Moving AngosturaBitter to Angostura class

--- src/ressacasbar/drinks/adicionais/condimentos/Angostura.java
+++ src/ressacasbar/drinks/adicionais/condimentos/Angostura.java
@@ -3,11 +3,11 @@ package ressacasbar.drinks.adicionais.condimentos;
 import ressacasbar.Drink;
 import ressacasbar.DrinkDecorator;
 
-public class AngosturaBitter extends DrinkDecorator{
+public class Angostura extends DrinkDecorator{
 
 	Drink drink;
 	
-	public AngosturaBitter(Drink drink) {
+	public Angostura(Drink drink) {
 		this.drink = drink;
 	}
 	public String getDescricao() {
